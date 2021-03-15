f806fbdde3bfc2692ad18dccd0da9e601ed88e66
nammae <tuannghia.tn@gmail.com>
2021-03-15 22:41:47 +0700

    first commit

---
+++ src/app/article/article.model.ts
@@ -0,0 +1,21 @@
+export class Article {
+    title: string;
+    link: string;
+    votes: number;
+
+    constructor(title: string, link: string, votes?: number) {
+        this.title = title;
+        this.link = link;
+        this.votes = votes || 0;
+    }
+
+    voteUp() : boolean {
+        this.votes += 1;
+        return false;
+      }
+    
+    voteDown() : boolean {
+        this.votes -= 1;
+        return false;
+    }
+}
